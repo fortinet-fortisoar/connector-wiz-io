ea8e53e9bff91f36954eedcaa601057eeba10b66
randomstr1ng <julian@petersohn.it>
2024-07-18 16:34:25 +0200

    update variables and operations to aligne with vendor docs

--- wiz-io/operations.py
+++ wiz-io/operations.py
@@ -7,22 +7,76 @@
 
 from connectors.core.connector import get_logger, ConnectorError
 from .constants import *
-from .utils import api_query_call
+from .utils import api_query_call, check_empty_value
 
 logger = get_logger(LOGGER_NAME)
 
 
 def get_issues(config, params):
     variables = {
-        "first": params.get("limit"),
-        # "after": "<pagenation hash>",
-        "orderBy": {
-            "field": "SEVERITY",
-            "direction": "DESC"
-        },
-        # Example Query: {"status": ["RESOLVED"]}
-        "filterBy": params.get("filterQuery")
-    }
+    "first":params.get("limit"),
+    "filterBy": {
+      "relatedEntity": {},
+      "createdAt": {},
+      "resolvedAt": {},
+    },
+  }
+    if params.get("pagination"):
+      pagination = {"after": params.get("pagination")}
+      variables.update(pagination)
+
+    if params.get("issueID"):
+      issueid = {"id": check_empty_value(params.get("issueID"))}
+      variables["filterBy"].update(issueid)
+
+    if params.get("searchQuery"):
+      searchQuery = {"search": params.get("searchQuery")}
+      variables["filterBy"].update(searchQuery)
+
+    if params.get("projectID"):
+      projectID = {"project": [params.get("projectID")]}
+      variables["filterBy"].update(projectID)
+
+    if params.get("severity"):
+      severity = {"severity": [params.get("severity")]}
+      variables["filterBy"].update(severity)
+
+    if params.get("status"):
+      status = {"status": [params.get("status")]}
+      variables["filterBy"].update(status)
+
+    if params.get("type"):
+      type_var = {"type": [params.get("type")]}
+      variables["filterBy"].update(type_var)
+
+    if params.get("relatedEntityID"):
+      relatedEntityID = {"id": params.get("relatedEntityID")}
+      variables["filterBy"]["relatedEntity"].update(relatedEntityID)
+
+    if params.get("realtedEntityType"):
+      realtedEntityType = {"type": check_empty_value(params.get("realtedEntityType"))}
+      variables["filterBy"]["relatedEntity"].update(realtedEntityType)
+
+    if params.get("relatedCloudPlatform"):
+      relatedCloudPlatform = {"cloudPlatform": check_empty_value(params.get("relatedCloudPlatform"))}
+      variables["filterBy"]["relatedEntity"].update(relatedCloudPlatform)
+
+    if params.get("createdBefore"):
+      createdBefore = {"before": params.get("createdBefore")}
+      variables["filterBy"]["createdAt"].update(createdBefore)
+
+    if params.get("createdAfter"):
+      createdAfter = {"after": params.get("createdAfter")}
+      variables["filterBy"]["createdAt"].update(createdAfter)
+
+    if params.get("resolvedBefore"):
+      resolvedBefore = {"before": params.get("resolvedBefore")}
+      variables["filterBy"]["resolvedAt"].update(resolvedBefore)
+
+    if params.get("resolvedAfter"):
+      resolvedAfter = {"after": params.get("resolvedAfter")}
+      variables["filterBy"]["resolvedAt"].update(resolvedAfter)
+
     try:
         response = api_query_call(config, query=GET_ISSUES_QUERY, variables=variables)
         return response.json()
@@ -31,14 +85,28 @@ def get_issues(config, params):
         raise ConnectorError(err)
 
 
+
 def get_inventory_assets(config, params):
     variables = {
-        "first": params.get("limit"),
-        # "after": "<pagenation hash>",
-        "projectId": params.get("projectID"),
-        # Example Query: {"type": ["VIRTUAL_MACHINE"], "where": {"cloudPlatform": {"EQUALS": ["AWS"]}}}
-        "query": params.get("filterQuery")
+    "first":params.get("limit"),
+    "filterBy": {
+      "projectId": [
+        params.get("projectID")
+      ],
+      "type": [
+        params.get("type")
+      ],
+      "updatedAt": {
+        "before":params.get("updatedBefore"),
+        "after":params.get("updatedAfter"),
+      },
+      "deletedAt": {
+        "before":params.get("deletedBefore"),
+        "after":params.get("deletedAfter"),
+      },
+      "search":params.get("searchTerm")
     }
+  }
     try:
         response = api_query_call(config, query=GET_INVENTORY_ASSETS_QUERY, variables=variables)
         return response.json()
@@ -46,47 +114,16 @@ def get_inventory_assets(config, params):
         logger.exception(err)
         raise ConnectorError(err)
 
-
-def get_issues_by_asset(config, params):
-    variables = {
-        "first": params.get("limit"),
-        "fetchIssues": True,
-        "filterBy": {
-            "project": [params.get("projectID")],
-            "status": params.get("status"),
-            "relatedEntity": {
-                "id": params.get("assetID")
-            },
-            "sourceRule": {}
-        },
-        "orderBy": {
-            "field": "SEVERITY",
-            "direction": "DESC"
-        },
-        "groupBy": "SOURCE_RULE",
-        "groupOrderBy": {
-            "field": "SEVERITY",
-            "direction": "DESC"
-        }
-    }
-
-    try:
-        response = api_query_call(config, query=GET_ISSUES_BY_ASSET_QUERY, variables=variables)
-        return response.json()
-    except Exception as err:
-        logger.exception(err)
-        raise ConnectorError(err)
-
-
 def get_projects(config, params):
     variables = {
-        "first": params.get("limit"),
-        "filterBy": params.get("filterQuery"),
-        "orderBy": {
-            "field": "IS_FOLDER",
-            "direction": "DESC"
-        }
-    }
+    "first": params.get("limit"),
+    "filterBy": {
+      "search": params.get("name"),
+      "includeArchived": params.get("includeArchivedProjects"),
+      "root": True,
+      "impact": params.get("businessImpact"),
+    },
+  }
     try:
         response = api_query_call(config, query=GET_PROJECTS_QUERY, variables=variables)
         return response.json()
@@ -109,13 +146,42 @@ def add_comment_to_issue(config, params):
         logger.exception(err)
         raise ConnectorError(err)
 
-def get_vulnerabilities_for_asset(config, params):
+def get_vulnerabilities(config, params):
     variables = {
-        "first": params.get("limit"),
-        "filterBy": {
-            "assetId": [params.get("assetID")]
-        }
-    }
+   "first":params.get("limit"),
+   "filterBy":{
+      "id":check_empty_value(params.get("vulnerabilityID")),
+       "subscriptionExternalId":check_empty_value(params.get("externalSubscriptionID")),
+      "assetType":[
+         params.get("assetType")
+      ],
+      "vendorSeverity":[
+         params.get("severity")
+      ],
+      "status":[
+         params.get("status")
+      ],
+      "assetId":check_empty_value(params.get("assetID")),
+      "vulnerabilityId":check_empty_value(params.get("vulnerabilityID")),
+      "firstSeenAt":{
+         "before":params.get("firstSeenBefore"),
+         "after":params.get("firstSeenAfter"),
+      },
+      "resolvedAt":{
+         "before":params.get("resolvedBefore"),
+         "after":params.get("resolvedAfter"),
+      },
+      "projectId":[
+         params.get("projectID"),
+      ],
+      "hasFix":params.get("patchAvailable"),
+      "hasExploit":params.get("exploitAvailable"),
+   }
+}
+    if len(params.get("pagination")) > 0:
+      pagination = {"after": params.get("pagination")}
+      variables.update(pagination)
+
     try:
         response = api_query_call(config, query=GET_VULNERABILITIES_FOR_ASSET, variables=variables)
         return response.json()
@@ -123,12 +189,10 @@ def get_vulnerabilities_for_asset(config, params):
         logger.exception(err)
         raise ConnectorError(err)
 
-
 operations = {
     'get_issues': get_issues,
     'get_inventory_assets': get_inventory_assets,
-    'get_issues_by_asset': get_issues_by_asset,
     'get_projects': get_projects,
     'add_comment_to_issue': add_comment_to_issue,
-    'get_vulnerabilities_for_asset': get_vulnerabilities_for_asset
+    'get_vulnerabilities': get_vulnerabilities,
 }
